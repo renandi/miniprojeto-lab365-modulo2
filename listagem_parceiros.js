8506c4f06d9e6d3b4586bfb38e4633192420a37e
Renan Nandi <renannandi@gmail.com>
2025-07-01 21:06:41 -0300

    Adiciona funcionalidade de filtrar os locais por nome ou bairro.

--- listagem_parceiros.js
+++ listagem_parceiros.js
@@ -1,57 +1,91 @@
-
 var container = document.getElementById("cards-container");
 
 function createCard(obj) {
+  var card = document.createElement("card");
+  card.id = "card-" + obj.id;
 
-    var card = document.createElement("card");
-    
-    var avatar = document.createElement("p");
-    avatar.innerText = obj.tipoParceiro;
+  var avatar = document.createElement("p");
+  avatar.innerText = obj.tipoParceiro;
 
-    var img = document.createElement("img");
-    
-    switch (obj.tipoParceiro) {
-        case "ECO":
-            img.src = "images/ecoponto-pin.png";
-            break;
-        case "COO":
-            img.src = "images/shake-hands-logo.png";
-            break;
-        case "PEVs": 
-            img.src = "images/icons8-sinal-de-reciclagem-64.png";
-            break;
-    }
-    
-    img.classList.add("avatar");
+  var img = document.createElement("img");
 
-    var nomeText = document.createElement("p");
-    nomeText.innerText = obj.nomeParceiro;
+  switch (obj.tipoParceiro) {
+    case "ECO":
+      img.src = "images/ecoponto-pin.png";
+      break;
+    case "COO":
+      img.src = "images/shake-hands-logo.png";
+      break;
+    case "PEVs":
+      img.src = "images/icons8-sinal-de-reciclagem-64.png";
+      break;
+  }
 
-    var bairroText = document.createElement("p");
-    bairroText.innerText = obj.bairro;
+  img.classList.add("avatar");
 
-    var dataInclusaoText = document.createElement("p");
-    dataInclusaoText.innerText = obj.dataCriacao;;
+  var nomeText = document.createElement("p");
+  nomeText.innerText = obj.nomeParceiro;
 
-    card.appendChild(avatar);
-    card.appendChild(img);
-    card.appendChild(nomeText);
-    card.appendChild(bairroText);
-    card.appendChild(dataInclusaoText);
+  var bairroText = document.createElement("p");
+  bairroText.innerText = obj.bairro;
 
-    container.appendChild(card);
-    card.classList.add("card");
+  var dataInclusaoText = document.createElement("p");
+  dataInclusaoText.innerText = obj.dataCriacao;
+
+  card.appendChild(avatar);
+  card.appendChild(img);
+  card.appendChild(nomeText);
+  card.appendChild(bairroText);
+  card.appendChild(dataInclusaoText);
+
+  container.appendChild(card);
+  card.classList.add("card");
 }
 
-
 fetch("https://6860899b8e74864084437167.mockapi.io/jmt-futurodev/api/parceiros")
-.then(response => response.json())
-.then(parceiros => {
+  .then((response) => response.json())
+  .then((parceiros) => {
     // console.log(parceiros);
-    parceiros.forEach(parceiro => {
-        if (parceiro.id>2) createCard(parceiro);
+    parceiros.forEach((parceiro) => {
+      if (parceiro.id > 2) createCard(parceiro);
     });
-})
-.catch(error => {
+  })
+  .catch((error) => {
     console.log("Ocorreu um erro ao buscar os parceiros", error);
-})
+  });
+
+
+const searchInput = document.getElementById("searchText");
+const search = document.getElementById("searchBtn");
+search.addEventListener ("click", () => {
+  fetch(
+    "https://6860899b8e74864084437167.mockapi.io/jmt-futurodev/api/parceiros"
+  )
+    .then((response) => response.json())
+    .then((parceiros) => {
+
+    parceiros.forEach((parceiro) => {
+        if ((parceiro.nomeParceiro.toLowerCase().includes(searchInput.value.toLowerCase()) || parceiro.bairro.toLowerCase().includes(searchInput.value.toLowerCase())))
+            showCard(parceiro.id);
+        else hideCard(parceiro.id);
+      });
+    })
+    .catch((error) => {
+      console.log("Ocorreu um erro ao buscar os parceiros", error);
+    });
+});
+
+
+function hideCard(parceiroId) {
+    const card = document.getElementById("card-"+parceiroId);
+    if (card) {
+        card.style.display = "none";
+    }
+}
+
+function showCard (id) {
+    const card = document.getElementById("card-"+id);
+    if (card) {
+        card.style.display = "flex";
+    }
+}
